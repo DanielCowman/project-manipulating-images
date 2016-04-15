c9d25a6e58f42193fa6ee505312e0d44186ea147
DanielCowman <dcowman@mail.valenciacollege.edu>
2016-04-14 20:50:40 -0400

    change to if statement

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -21,16 +21,17 @@ for filename in IMG_FILEPATH:
 
     if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
           # Calculate the new width and height to resize to.
-          if width > height:
-            height = int((SQUARE_FIT_SIZE / width) * height)
-            width = SQUARE_FIT_SIZE
-          else:
-            width = int((SQUARE_FIT_SIZE / height) * width)
-            height = SQUARE_FIT_SIZE
+      if width > height:
+        height = int((SQUARE_FIT_SIZE / width) * height)
+        width = SQUARE_FIT_SIZE
+      else:
+        width = int((SQUARE_FIT_SIZE / height) * width)
+        height = SQUARE_FIT_SIZE
 
-    # Resize the image.
-    print('Resizing %s...' % (filename))
-    origIm = origIm.resize((width, height))
+      # Resize the image.
+      print('Resizing %s...' % (filename))
+      origIm = origIm.resize((width, height))
+
     # Add the logo.
     print('Adding logo to %s...' % (filename))
     origIm.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
